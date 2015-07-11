0eee5613abceb178169f12f6d4a448ec58b1d1df
electrojustin <electrojustin@gmail.com>
2015-07-11 17:34:45 -0400

    Got ELF injection working with statically linked files

--- src/elf_inject.c
+++ src/elf_inject.c
@@ -16,9 +16,8 @@ blob elf_inject (char* code, size_t code_size, char* elf_buf, size_t elf_buf_siz
 	Elf64_Phdr* new_program_tables;
 	int i;
 	uint64_t entry_point = file_header->e_entry;
-	uint64_t current_offset;
 	Elf64_Phdr new_segment;
-	Elf64_Phdr* last_loadable = NULL;
+	Elf64_Phdr* first_loadable = NULL;
 	Elf64_Phdr* text_segment;
 	blob ret;
 
@@ -26,52 +25,44 @@ blob elf_inject (char* code, size_t code_size, char* elf_buf, size_t elf_buf_siz
 	{
 		if (program_tables [i].p_vaddr < entry_point && program_tables [i].p_vaddr + program_tables [i].p_memsz > entry_point)
 			text_segment = (Elf64_Phdr*)(new_elf_buf + program_tables [i].p_offset);
-		if (program_tables [i].p_type == PT_LOAD && (!last_loadable || program_tables [i].p_vaddr > last_loadable->p_vaddr))
-			last_loadable = &program_tables [i];
+		if (program_tables [i].p_type == PT_LOAD && !first_loadable)
+			first_loadable = &program_tables [i];
+		if (text_segment && first_loadable)
+			break;
 	}
 
 	//Setup the new segment
 	new_segment.p_type = PT_LOAD;
-	//Shoutout to the folks on freenode's ##re channel for helping me figure out this alignment thing!
 	new_segment.p_offset = elf_buf_size;
-	new_segment.p_vaddr = ceil (last_loadable->p_vaddr / (double)getpagesize ()) * getpagesize() + new_segment.p_offset;
+	new_segment.p_vaddr = first_loadable->p_vaddr + new_segment.p_offset;
 	new_segment.p_paddr = new_segment.p_vaddr;
-	new_segment.p_filesz = code_size;
-	new_segment.p_memsz = code_size;
+	new_segment.p_filesz = code_size + (file_header->e_phnum + 1) * file_header->e_phentsize;
+	new_segment.p_memsz = new_segment.p_filesz;
 	new_segment.p_flags = PF_R | PF_X;
 	new_segment.p_align = getpagesize ();
 
 	//Allocate memory for the new segment
-	new_elf_buf = (char*)malloc (new_segment.p_offset + code_size);
-	ret.buf_size = new_segment.p_offset + code_size;
+	new_elf_buf = (char*)malloc (new_segment.p_offset + code_size + (file_header->e_phnum +1) * file_header->e_phentsize);
+	ret.buf_size = new_segment.p_offset + code_size + (file_header->e_phnum + 1) * file_header->e_phentsize;
 	ret.buf = new_elf_buf;
 
-	//Copy everything up to the end of the program tables
-	memcpy (new_elf_buf, elf_buf, file_header->e_phoff + sizeof (Elf64_Phdr)*file_header->e_phnum);
-	current_offset = file_header->e_phoff + sizeof (Elf64_Phdr)*file_header->e_phnum;
+	//Copy the ELF
+	memcpy (new_elf_buf, elf_buf, elf_buf_size);
 
-	//Modify the offsets on all the segments since we're adding a new segment
-	new_program_tables = (Elf64_Phdr*)(new_elf_buf + file_header->e_phoff);
-	for (i = 0; i < file_header->e_phnum; i ++)
-	{
-		if (new_program_tables [i].p_offset > file_header->e_phoff)
-			new_program_tables [i].p_offset += sizeof (Elf64_Phdr);
-	}
+	//Copy the old program headers to the end of the file
+	memcpy (new_elf_buf + elf_buf_size, elf_buf + file_header->e_phoff, file_header->e_phnum * file_header->e_phentsize);
 
-	//Copy the new segment
-	memcpy (new_elf_buf + current_offset, &new_segment, sizeof (Elf64_Phdr));
-	current_offset += sizeof (Elf64_Phdr);
-
-	//Copy the rest of the elf
-	memcpy (new_elf_buf + current_offset, elf_buf + current_offset - sizeof (Elf64_Phdr), elf_buf_size - current_offset - sizeof (Elf64_Phdr));
+	//Copy the new segment to the end of the new program header table
+	memcpy (new_elf_buf + elf_buf_size + file_header->e_phnum * file_header->e_phentsize, &new_segment, sizeof (Elf64_Phdr));
 
 	//Copy the new code
-	memcpy (new_elf_buf + new_segment.p_offset, code, code_size);
+	memcpy (new_elf_buf + ret.buf_size - code_size, code, code_size);
 
 	//Modify the ELF header
 	new_file_header = (Elf64_Ehdr*)new_elf_buf;
 	new_file_header->e_phnum ++;
-	new_file_header->e_entry = new_segment.p_vaddr;
+	new_file_header->e_phoff = elf_buf_size;
+	new_file_header->e_entry = ret.buf_size - code_size + first_loadable->p_vaddr;
 	new_file_header->e_shoff = 0;
 	new_file_header->e_shnum = 0;
 	new_file_header->e_shentsize = 0;
--- tests/injection_test.c
+++ tests/injection_test.c
@@ -9,7 +9,7 @@ int main (int argc, char** argv)
 	size_t input_file_size;
 	char* input_buf;
 	blob output;
-	char code [6] = { 0xFF, 0x2D, 0x00, 0x80, 0x04, 0x08 };
+	char code [7] = { 0xBD, 0x20, 0x0C, 0x40, 0x00, 0xFF, 0xE5 };
 
 	input_file = fopen (argv [1], "r");
 	fseek (input_file, 0, SEEK_END);
@@ -19,8 +19,8 @@ int main (int argc, char** argv)
 	fread (input_buf, 1, input_file_size, input_file);
 	fclose (input_file);
 
-	*(int*)(&code [2]) = ((Elf64_Ehdr*)input_buf)->e_entry;
-	output = elf_inject (code, 6, input_buf, input_file_size);
+	*(int*)(&code [1]) = ((Elf64_Ehdr*)input_buf)->e_entry;
+	output = elf_inject (code, 7, input_buf, input_file_size);
 
 	output_file = fopen (argv [2], "w");
 	fwrite (output.buf, output.buf_size, 1, input_file);
